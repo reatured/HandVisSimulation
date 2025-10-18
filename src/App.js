e4851896f96eee42fabb8afbc907c8e9b5ff2d15
Lingyi Zhou <61943376+reatured@users.noreply.github.com>
2025-10-18 16:16:40 -0700

    Add 3D assets

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 
@@ -28,7 +28,7 @@ function Box(props) {
 export default function App() {
   return (
     <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
+      <ambientLight intensity={Math.PI } />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
       <Box position={[-1.2, 0, 0]} />
