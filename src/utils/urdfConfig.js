fe4cb1ab57297c6616114e1bd3cdcb5631c03400
Lingyi Zhou <61943376+reatured@users.noreply.github.com>
2025-10-18 20:52:18 -0700

    Fix asset paths for GitHub Pages deployment

--- src/utils/urdfConfig.js
+++ src/utils/urdfConfig.js
@@ -4,37 +4,39 @@
  * Using GLB versions for better web performance
  */
 
+const PUBLIC_URL = process.env.PUBLIC_URL || ''
+
 export const URDF_MODELS = {
   ability_hand: {
-    left: '/assets/robots/hands/ability_hand/ability_hand_left_glb.urdf',
-    right: '/assets/robots/hands/ability_hand/ability_hand_right_glb.urdf',
+    left: `${PUBLIC_URL}/assets/robots/hands/ability_hand/ability_hand_left_glb.urdf`,
+    right: `${PUBLIC_URL}/assets/robots/hands/ability_hand/ability_hand_right_glb.urdf`,
   },
   shadow_hand: {
-    left: '/assets/robots/hands/shadow_hand/shadow_hand_left_glb.urdf',
-    right: '/assets/robots/hands/shadow_hand/shadow_hand_right_glb.urdf',
+    left: `${PUBLIC_URL}/assets/robots/hands/shadow_hand/shadow_hand_left_glb.urdf`,
+    right: `${PUBLIC_URL}/assets/robots/hands/shadow_hand/shadow_hand_right_glb.urdf`,
   },
   allegro_hand: {
-    left: '/assets/robots/hands/allegro_hand/allegro_hand_left_glb.urdf',
-    right: '/assets/robots/hands/allegro_hand/allegro_hand_right_glb.urdf',
+    left: `${PUBLIC_URL}/assets/robots/hands/allegro_hand/allegro_hand_left_glb.urdf`,
+    right: `${PUBLIC_URL}/assets/robots/hands/allegro_hand/allegro_hand_right_glb.urdf`,
   },
   leap_hand: {
-    left: '/assets/robots/hands/leap_hand/leap_hand_left_glb.urdf',
-    right: '/assets/robots/hands/leap_hand/leap_hand_right_glb.urdf',
+    left: `${PUBLIC_URL}/assets/robots/hands/leap_hand/leap_hand_left_glb.urdf`,
+    right: `${PUBLIC_URL}/assets/robots/hands/leap_hand/leap_hand_right_glb.urdf`,
   },
   schunk_hand: {
-    left: '/assets/robots/hands/schunk_hand/schunk_svh_hand_left_glb.urdf',
-    right: '/assets/robots/hands/schunk_hand/schunk_svh_hand_right_glb.urdf',
+    left: `${PUBLIC_URL}/assets/robots/hands/schunk_hand/schunk_svh_hand_left_glb.urdf`,
+    right: `${PUBLIC_URL}/assets/robots/hands/schunk_hand/schunk_svh_hand_right_glb.urdf`,
   },
   dclaw_gripper: {
-    null: '/assets/robots/hands/dclaw_gripper/dclaw_gripper_glb.urdf',
+    null: `${PUBLIC_URL}/assets/robots/hands/dclaw_gripper/dclaw_gripper_glb.urdf`,
   },
   barrett_hand: {
     // Barrett hand may not have GLB version, will need to check
-    null: '/assets/robots/hands/barrett_hand/barrett_hand.urdf',
+    null: `${PUBLIC_URL}/assets/robots/hands/barrett_hand/barrett_hand.urdf`,
   },
   panda_gripper: {
     // Panda gripper may not have GLB version, will need to check
-    null: '/assets/robots/hands/panda_gripper/panda_gripper.urdf',
+    null: `${PUBLIC_URL}/assets/robots/hands/panda_gripper/panda_gripper.urdf`,
   },
 }
 
